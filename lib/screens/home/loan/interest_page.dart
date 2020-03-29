c87290acf5f0942946cf3ab629e71e40711d9cec
Bhawna Paliwal <bhawna7374@gmail.com>
2020-03-29 21:45:09 +0530

    compound interest option

--- lib/screens/home/loan/interest_page.dart
+++ lib/screens/home/loan/interest_page.dart
@@ -2,7 +2,7 @@
 import 'package:farm_expense_management/common/ui/pal_button.dart';
 import 'package:flutter/material.dart';
 import 'package:farm_expense_management/common/ui/pal_title_view.dart';
-
+import 'dart:math';
 class InterestPage extends StatefulWidget{
   @override
   _InterestPageState createState() => _InterestPageState();
@@ -11,14 +11,21 @@ class InterestPage extends StatefulWidget{
 class _InterestPageState extends State<InterestPage>{
   double interest =0;
   double amount = 0;
-  double calculateInterest(int principal, int rate, int months)
+  int selectedOption=0;// 0 = simple interest, 1 = compound interest
+  double calculateSimpleInterest(int principal, int rate, int months)
   {
     print('check32');
     double rate1 = (rate*1.0)/12;
     double to_ret = (rate1*principal*months*1.0)/100;
     return to_ret;
   }
-  
+  double calculateCompoundInterest(int principal, int rate, int months)
+  {
+    //print('check32');
+    double amount = principal*pow(1+(rate*1.0)/1200,months);
+    double to_ret = amount-principal;
+    return to_ret;
+  }
   final primaryColor = const Color(0xFFFFFFFF);
   final _formKey = GlobalKey<FormState>();
   final TextEditingController princController =
@@ -36,7 +43,11 @@ class _InterestPageState extends State<InterestPage>{
   }
   void _onPressed()
   {
-    double _interest  = calculateInterest(int.parse(princController.text), int.parse(roiController.text), int.parse(monthsController.text));
+    double _interest=0;
+    if (selectedOption == 0)
+      _interest = calculateSimpleInterest(int.parse(princController.text), int.parse(roiController.text), int.parse(monthsController.text));
+    else
+      _interest = calculateCompoundInterest(int.parse(princController.text), int.parse(roiController.text), int.parse(monthsController.text));
     setState(() {
       amount = int.parse(princController.text) + _interest;
       interest = _interest;
@@ -126,6 +137,26 @@ class _InterestPageState extends State<InterestPage>{
                         fontSize: 18.0
                       ),),
                     ),
+                    ListTile(
+                      title: const Text('Simple Interest'),
+                      leading: Radio(
+                        value: 0,
+                        groupValue: selectedOption,
+                        onChanged: (int value) {
+                          setState(() { selectedOption = value; });
+                        },
+                      ),
+                    ),
+                    ListTile(
+                      title: const Text('Compound Interest'),
+                      leading: Radio(
+                        value: 1,
+                        groupValue: selectedOption,
+                        onChanged: (int value) {
+                          setState(() { selectedOption = value; });
+                        },
+                      ),
+                    ),
                     Padding(
                       padding: EdgeInsets.all(16.0),
                       child: PalButton(
@@ -138,7 +169,7 @@ class _InterestPageState extends State<InterestPage>{
                   ],
                 ),
               ),
-            )
+            ),
           ],
         ),
       ),
